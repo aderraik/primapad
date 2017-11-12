e6cb852b0b873cc151e7a8b4d5efdad9c6b6ce74
Andre B. Derraik <andre@tecgraf.puc-rio.br>
2017-11-12 17:26:05 -0200

    Updating start script

--- run.sh
+++ run.sh
@@ -2,14 +2,17 @@
 
 SERVER_PORT=80
 PRIMAPAD_HOME=/root/primapad
+LOG_FILE=/var/log/primapad.log
+PID_FILE=/run/primapad.pid
+VERSION=0.1
 
 java \
 	-Dgrails.env=prod \
 	-Dserver.port=$SERVER_PORT \
-	-jar $PRIMAPAD_HOME/build/libs/primapad-0.1.war \
+	-jar $PRIMAPAD_HOME/build/libs/primapad-$VERSION.war \
 	-server -Xmx768M -XX:MaxPermSize=256m \
-	 > /var/log/primapad.log 2>&1 & \
-	 echo $! > /run/primapad.pid
+	 > $LOG_FILE 2>&1 & \
+	 echo $! > $PID_FILE
 
 exit 0
 
